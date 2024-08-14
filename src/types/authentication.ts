f9dbaaafe1eb45c1c2368c5886557da86776b897
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-08-14 23:06:05 +0200

    feat(types): add authentication type definition

---
+++ src/types/authentication.ts
@@ -0,0 +1,23 @@
+export interface rawAuthResult {
+  /** Bearer token */
+  access_token: string;
+  /** ID of the account */
+  userId: number;
+  /** ID of the host */
+  hoteId: number;
+}
+
+export interface AuthCredentials {
+  /** Bearer Token */
+  token: string;
+  /** ID of the host */
+  hostId: number;
+  /** ID of the account */
+  userId: number;
+  /** Username used for authentication */
+  username: string | null;
+  /** Password used for authentication */
+  password: string | null;
+  /** Token expiration date */
+  token_expiry: number;
+}
