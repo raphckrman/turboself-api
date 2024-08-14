fe065f0c2303b1deeb3555365dcc94fbddf0fa9e
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-08-14 23:45:14 +0200

    feat(index): add exports

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export * from "./constants";
+
+export * from "./routes/Host";
+export * from "./routes/Account";
+export * from "./routes/Establishment";
+export * from "./routes/Payment";
+
+export * from "./structures/Client";
+export * from "./structures/Establishment";
+export * from "./structures/Host";
+export * from "./structures/Balance";
+export * from "./structures/Payment";
+
+export * from "./types/account";
+export * from "./types/authentication";
+export * from "./types/establishment";
+export * from "./types/host";
+export * from "./types/request-handler";
+
+export * from "./utils/authenticate";
