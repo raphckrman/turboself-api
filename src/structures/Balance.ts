888ff22069cddc9cb76d6db006d6e9f705908720
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-08-14 23:36:08 +0200

    feat(structures): add Balance structure

---
+++ src/structures/Balance.ts
@@ -0,0 +1,14 @@
+export class Balance {
+  constructor(
+    /** Internal identifier of a host's accounting in the digital space */
+    public id: number,
+    /** Amount of the balance */
+    public amount: number,
+    /** Estimated host account balance (taking into account future reservations and OK payments) */
+    public estimatedAmount: number,
+    /** Date of the estimated balance */
+    public estimatedDate: Date
+  ) {
+
+  }
+}
