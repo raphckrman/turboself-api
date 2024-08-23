509241d23733897e704e74f5e406c558f3391d48
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-08-23 15:04:52 +0200

    feat(authenticate): add a method to log in with a ticket

--- src/utils/authenticate.ts
+++ src/utils/authenticate.ts
@@ -45,3 +45,48 @@ export const authenticateWithCredentials = async (
         token_expiry: Date.now() + 3300000
     }, establishment, host, balance);
 };
+
+export const authenticateWithTicket = async (
+    ent: "PRONOTE" | string,
+    ticket: string,
+    remember = true,
+    minimalist = false
+): Promise<Client> => {
+    const response = await fetch(`https://espacenumerique.turbo-self.com/Connexion.aspx?ent=${ent}&ticket=${ticket}`);
+
+    const cookies = response.headers.get("set-cookie");
+    const username: string = cookies?.split(";")[0].split("=")[1] ?? "";
+    const password: string = cookies?.split(";")[0].split("=")[1] ?? "";
+
+    const data = await manager.post<rawAuthResult>(AUTH_LOGIN(), {
+        username,
+        password
+    });
+
+    if (minimalist) {
+        return new Client({
+            token:        data.access_token,
+            hostId:       data.hoteId,
+            userId:       data.userId,
+            username:     remember ? username : null,
+            password:     remember ? password : null,
+            token_expiry: Date.now() + 3300000
+        });
+    }
+
+    const host = await getHost(data.access_token, data.hoteId);
+
+    const [balance, establishment] = await Promise.all([
+        getBalances(data.access_token, data.hoteId),
+        getEstablishment(data.access_token, host.etabId)
+    ]);
+
+    return new Client({
+        token:        data.access_token,
+        hostId:       data.hoteId,
+        userId:       data.userId,
+        username:     remember ? username : null,
+        password:     remember ? password : null,
+        token_expiry: Date.now() + 3300000
+    }, establishment, host, balance);
+};
